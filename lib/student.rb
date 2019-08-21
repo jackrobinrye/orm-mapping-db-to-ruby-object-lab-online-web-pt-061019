43e45db48340cb083368f375dd0cf40ff8ff57e2
Jack Atherton <jackrrye@gmail.com>
2019-08-21 21:07:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -49,14 +49,14 @@ class Student
     DB[:conn].execute(sql)
   end
 
-  def self.all_students_below_12th_grade 
+  def self.all_students_below_12th_grade
     sql = <<-SQL
-    SELECT* 
-    FROM students 
+    SELECT*
+    FROM students
     WHERE grade < 12
     SQL
     DB[:conn].execute(sql)
-  end 
+  end
 
 
 
