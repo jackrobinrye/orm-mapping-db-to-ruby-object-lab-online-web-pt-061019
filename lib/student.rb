a2a4dac2fa28b702a034e04268364dee37fdb24b
Jack Atherton <jackrrye@gmail.com>
2019-08-21 20:56:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,14 +40,14 @@ class Student
     student
   end
 
-  # def self.all_students_in_grade_9
-  #   sql = <<-SQL
-  #   SELECT *
-  #   FROM students
-  #   WHERE grade = 9
-  #   SQL
-  #   DB[:conn].execute(sql)
-  # end
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = 9
+    SQL
+    DB[:conn].execute(sql)
+  end
 
 
 
