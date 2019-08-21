06cbdc93b018f49f15b33e483243bdde78399444
Jack Atherton <jackrrye@gmail.com>
2019-08-21 21:07:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -49,6 +49,15 @@ class Student
     DB[:conn].execute(sql)
   end
 
+  def self.all_students_below_12th_grade 
+    sql = <<-SQL
+    SELECT* 
+    FROM students 
+    WHERE grade < 12
+    SQL
+    DB[:conn].execute(sql)
+  end 
+
 
 
   def save
