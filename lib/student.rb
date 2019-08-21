b39c1d91667ebcf2696a5c56771bc02a39f4c58a
Jack Atherton <jackrrye@gmail.com>
2019-08-21 21:17:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -68,7 +68,9 @@ class Student
     WHERE grade = 10
     LIMIT ?
     SQL
-    DB[:conn].execute(sql, x)
+    DB[:conn].execute(sql, x).map do |row|
+      self.new_from_db(row)
+    end 
   end
 
 
