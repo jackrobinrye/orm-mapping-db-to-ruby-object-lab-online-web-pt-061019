da4c955e2f848dfed275b6ddb7b8d32285081447
Jack Atherton <jackrrye@gmail.com>
2019-08-21 20:48:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,7 +29,7 @@ class Student
     FROM students
     WHERE name = ?
     SQL
-    # DB[:conn].execute(sql, name).map |row|
+    # DB[:conn].execute(sql, name).map do |row|
     #   self.new_from_db(row)
     # end
   end
